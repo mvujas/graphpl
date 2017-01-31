0e605f308d9ea3d7293684ec35818617cb8b09d4
mvujas <milos.vujasinovic.98@gmail.com>
2017-01-31 22:26:53 +0100

    struct.pl changes

--- struct.pl
+++ struct.pl
@@ -7,7 +7,7 @@ init(Struct):- Struct = [].
 push_first(Element, Struct, [ Element | Struct ]).
 
 % Push element at the bottom of structure
-push_last(Element, [], [ Element ]).
+push_last(Element, [], [ Element ]):- !.
 push_last(Element, [ Head | Rest ], [ Head | Struct ]):-
 	push_last(Element, Rest, Struct).
 
@@ -15,18 +15,18 @@ push_last(Element, [ Head | Rest ], [ Head | Struct ]):-
 pop_first([ Element | Struct ], Element, Struct).  
 
 % Return last element of structure and rest of it
-pop_last([ Element ], Element, []).
+pop_last([ Element ], Element, []):- !.
 pop_last([ Head | Struct ], Element, [ Head | Struct1 ]):- 
 	pop_last(Struct, Element, Struct1).
 
 % Push element in ascending order
-push_asc(Element, [], [Element]).
+push_asc(Element, [], [Element]):- !.
 push_asc(Element, [ Head | Rest ], [ Element, Head | Rest ]):- Element =< Head.
 push_asc(Element, [ Head | Rest ], [ Head | Result ]):- Element > Head,
 	push_asc(Element, Rest, Result).
 
 % Push element in descending order
-push_desc(Element, [], [Element]).
+push_desc(Element, [], [Element]):- !.
 push_desc(Element, [ Head | Rest ], [ Element, Head | Rest ]):- Element >= Head.
 push_desc(Element, [ Head | Rest ], [ Head | Result ]):- Element < Head,
 	push_desc(Element, Rest, Result).
